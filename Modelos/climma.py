587054c3aeed6e33796b393c84d65b8ea8b0b914
Alexander Medina <medina.alexander@correo.unimet.edu.ve>
2026-08-03 21:34:57 -0400

    clima prueba4

--- Modelos/climma.py
+++ Modelos/climma.py
@@ -2,5 +2,17 @@ class ClimaActual:
     """Guardar la información del clima actual de una localidad."""
 
     def __init__(self, temperatura, humedad, velocidad_viento, estado_tiempo):
-        """Guarda las magnitudes obtenidas de la API"""        
+        """Guarda las magnitudes obtenidas de la API"""
+        self.temperatura = temperatura
+        self.humedad = humedad
+        self.velocidad_viento = velocidad_viento
+        self.estado_tiempo = estado_tiempo
 
+    def __str__(self):
+        """Texto con los detalles del clima."""
+        texto = ""
+        texto += " - Temperatura actual: " + str(self.temperatura) + "C\n"
+        texto += " - Humedad: " + str(self.humedad) + "%\n"
+        texto += " - Velocidad del viento: " + str(self.velocidad_viento) + " km/h\n"
+        texto += " - Estado del tiempo: " + self.estado_tiempo
+        return texto
