acd6bb888212477963d11c39ed5737cc9f35858f
Alexander Medina <medina.alexander@correo.unimet.edu.ve>
2026-08-03 21:04:22 -0400

    No hice nada

---
+++ Modelos/climma.py
@@ -0,0 +1,6 @@
+class ClimaActual:
+    """Guardar la información del clima actual de una localidad."""
+
+    def __init__(self, temperatura, humedad, velocidad_viento, estado_tiempo):
+        """Guarda las magnitudes obtenidas de la API"""        
+
