1c77ea36b2742d92a5e79785136b3a9f1630679d
Ricardo Dos Ramos <ric@MacBook-Air-de-Ric.local>
2026-08-04 23:37:51 -0400

    aarreglo main

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ def main():
     """Carga los datos, muestra el reporte inicial y abre el menu."""
     cargador = CargadorDatos()
 
-try:
-municipios = cargador.cargar_desde_json("zonas_caracas.json")
-    cargador.mostrar_reporte_inicial(municipios)
+    try:
+        municipios = cargador.cargar_desde_json("zonas_caracas.json")
+        cargador.mostrar_reporte_inicial(municipios)
 
-    menu = MenuInterfaz(municipios)
-    menu.iniciar()
-except Exception as error
-    print("Ocurrio un error al iniciar el programa:", error)
+        menu = MenuInterfaz(municipios)
+        menu.iniciar()
+    except Exception as error:
+        print("Ocurrio un error al iniciar el programa:", error)
 
 main()
