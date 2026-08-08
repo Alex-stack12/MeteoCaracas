bfe5840a6ede30a87bf9b792b87a298fe6aaa9a4
Ricardo Dos Ramos <ric@MacBook-Air-de-Ric.local>
2026-08-07 23:52:05 -0400

    agregue decodificar_estado para traducir los codigos de clima a espanol

--- Servicios/api_meteo.py
+++ Servicios/api_meteo.py
@@ -109,3 +109,39 @@ class ServicioMeteo:
         if i < len(lista):
             return lista[i]
         return None
+
+        def decodificar_estado(self, codigo):
+        """Convierte el codigo del tiempo de la API en un texto en espanol."""
+        codigos = {
+            0: "Despejado",
+            1: "Mayormente despejado",
+            2: "Parcialmente nublado",
+            3: "Nublado",
+            45: "Niebla",
+            48: "Niebla con escarcha",
+            51: "Llovizna ligera",
+            53: "Llovizna moderada",
+            55: "Llovizna densa",
+            56: "Llovizna helada ligera",
+            57: "Llovizna helada densa",
+            61: "Lluvia ligera",
+            63: "Lluvia moderada",
+            65: "Lluvia fuerte",
+            66: "Lluvia helada ligera",
+            67: "Lluvia helada fuerte",
+            71: "Nevada ligera",
+            73: "Nevada moderada",
+            75: "Nevada fuerte",
+            77: "Granos de nieve",
+            80: "Chubascos ligeros",
+            81: "Chubascos moderados",
+            82: "Chubascos violentos",
+            85: "Chubascos de nieve ligeros",
+            86: "Chubascos de nieve fuertes",
+            95: "Tormenta electrica",
+            96: "Tormenta con granizo ligero",
+            99: "Tormenta con granizo fuerte"
+        }
+        if codigo in codigos:
+            return codigos[codigo]
+        return "Desconocido (codigo " + str(codigo) + ")"
